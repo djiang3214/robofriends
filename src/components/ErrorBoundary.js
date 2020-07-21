4bb7aa526b58de5d6c1ac7592b8a64dd74962c57
Derek Jiang <djiang3214@gmail.com>
2020-07-21 19:26:46 -0400

    Robofriends Project

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+
+class ErrorBoundary extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch() {
+        this.setState({hasError: true})
+    }
+
+    render() {
+    if (this.state.hasError){
+        return<h1>Ooops. This is not good.</h1>
+    }
+    return this.props.children
+}
+}
+
+
+
+export default ErrorBoundary;
